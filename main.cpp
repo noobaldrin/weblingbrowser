bf57410703f3aef389cdb6a22f98876278659e8a
Aldrin Tadas <aldrintadas19@gmail.com>
2025-12-21 03:05:53 +0800

    Catch all exceptions in main

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
+#include <iostream>
+
+#include <SQLiteCpp/Exception.h>
 #include "Webling.h"
 
 int main(const int argc, char* argv[]) {
-    auto app = Webling::create("com.github.noobaldrin.webling",
-                                   Gio::Application::Flags::NONE);
-    return app->run(argc, argv);
+    try {
+        auto app = Webling::create("com.github.noobaldrin.webling",
+                                       Gio::Application::Flags::NONE);
+        return app->run(argc, argv);
+    } catch (const SQLite::Exception& e) {
+        std::cerr << e.what() << std::endl;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
 }
